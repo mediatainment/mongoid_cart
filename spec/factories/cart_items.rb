68d2429b970a1a0951691d464071ea47b0a09e4f
Jan Jezek <mail@mediatainment-productions.com>
2016-03-25 19:59:23 +0100

    car_item factory

--- spec/factories/cart_items.rb
+++ spec/factories/cart_items.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
 
-  factory MongoidCart::CartItem do
-
+  factory :cart_item, class: "MongoidCart::CartItem" do
+    sequence(:product_title) {|x| "Product Title #{x}"}
+    amount 10
+    unit %w{kg ml lt oz}.sample
+    type "Product"
+    net_price 123.45
   end
 end
