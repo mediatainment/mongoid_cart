b6417d8e609f0a848ead9dc2a4e3888673f163ab
Jan Jezek <mail@mediatainment-productions.com>
2016-03-30 20:22:16 +0200

    cart has_many cart_items

--- app/models/mongoid_cart/cart.rb
+++ app/models/mongoid_cart/cart.rb
@@ -8,6 +8,7 @@ module MongoidCart
     field :user_id, type: String
 
     has_many :cart_items, :inverse_of => :cart, :class_name => 'MongoidCart::CartItem'
+
     belongs_to :user, :inverse_of => :carts, :class_name => "User"
 
   end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -2,15 +2,20 @@ require 'rails_helper'
 
 describe MongoidCart::Cart do
 
-  describe 'add' do
+  describe 'relations' do
 
     before(:each) do
       @product = create(:product)
       @user = create(:user)
     end
 
-    it "has relation to user" do
+    it "belongs to User" do
       is_expected.to belong_to(:user).of_type(User).as_inverse_of(:carts)
+    end
+
+
+    it 'has_many cart_items' do
+      is_expected.to have_many(:cart_items).of_type(MongoidCart::CartItem).as_inverse_of(:cart)
     end
   end
 end
