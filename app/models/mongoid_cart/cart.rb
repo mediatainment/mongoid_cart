f30e8a0f689152c6509b5f4624b33e62637102d7
Jan Jezek <mail@mediatainment-productions.com>
2016-04-01 03:08:30 +0200

    User model relation to Cart as customer

--- app/models/mongoid_cart/cart.rb
+++ app/models/mongoid_cart/cart.rb
@@ -2,14 +2,12 @@
 
 module MongoidCart
   class Cart
-
     include Mongoid::Document
 
     field :user_id, type: String
 
     has_many :cart_items, :inverse_of => :cart, :class_name => 'MongoidCart::CartItem'
-
-    belongs_to :user, :inverse_of => :carts, :class_name => "User"
+    belongs_to :customer, :inverse_of => :carts, :class_name => 'User'
 
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -10,9 +10,8 @@ describe MongoidCart::Cart do
     end
 
     it "belongs to User" do
-      is_expected.to belong_to(:user).of_type(User).as_inverse_of(:carts)
+      is_expected.to belong_to(:customer).of_type(User).as_inverse_of(:carts)
     end
-
 
     it 'has_many cart_items' do
       is_expected.to have_many(:cart_items).of_type(MongoidCart::CartItem).as_inverse_of(:cart)
