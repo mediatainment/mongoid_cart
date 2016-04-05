dc88cf36366732c3fe145fd36e6ef18366157538
Jan Jezek <mail@mediatainment-productions.com>
2016-04-05 15:15:29 +0200

    removed customer_id

--- lib/mongoid_cart/acts_as_customer.rb
+++ lib/mongoid_cart/acts_as_customer.rb
@@ -10,16 +10,11 @@ module MongoidCart
     included do
 
       # a method which provides a customer_id
-      attr_accessor :customer_id, :current_cart
-
+      attr_accessor :current_cart
       has_many :carts, :class_name => 'MongoidCart::Cart', inverse_of: :customer
 
-      def customer_id
-        @customer_id ||= self.object_id.to_s
-      end
-
       def current_cart
-        carts.last || carts.new
+        carts.last || carts.create
       end
     end
 
