33a555a873dbdaa0c69b283920718bcb77b3cc7a
Jan Jezek <mail@mediatainment-productions.com>
2016-04-05 22:47:54 +0200

    cart_items better defined

--- spec/factories/cart_items.rb
+++ spec/factories/cart_items.rb
@@ -1,10 +1,14 @@
 FactoryGirl.define do
 
+  valid_units = %w{kg lt m pkg oz ml}
+
   factory :cart_item, class: "MongoidCart::CartItem" do
+    sequence(:sku) {|x| "PRODUCT-#{x}-SKU"}
     sequence(:product_title) {|x| "Product Title #{x}"}
-    amount 10
-    unit %w{kg ml lt oz}.sample
+    amount (1..100).to_a.sample
+    unit valid_units.sample
     type "Product"
-    net_price 123.45
+    net_price (1..10000).to_a.sample
+    cart
   end
 end
