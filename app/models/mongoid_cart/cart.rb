7ea318995e6b6723fcfd171fb3d61ac674ded60f
Jan Jezek <mail@mediatainment-productions.com>
2016-04-05 15:16:07 +0200

    static typed relation to product and persistance method "add" to add products and save them as items

--- app/models/mongoid_cart/cart.rb
+++ app/models/mongoid_cart/cart.rb
@@ -6,9 +6,17 @@ module MongoidCart
 
     field :user_id, type: String
 
+    belongs_to :customer, :inverse_of => :carts, :class_name => MongoidCart.configuration.customer_model_name
+
     has_many :cart_items, :inverse_of => :cart, :class_name => 'MongoidCart::CartItem'
 
-    belongs_to :customer, :inverse_of => :carts, :class_name => MongoidCart.configuration.customer_model_name
+    def products
+      Product.in(id: cart_items.pluck(:product_id))
+    end
+
+    def add(product_object)
+      cart_items.create!(product_object.to_cart_item_params)
+    end
 
   end
 end
